020ee3621f9993a33cd81a56d26b5641bf1be406
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-05-31 22:09:05 +0100

    adding random nr letter upper lower

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-console.log(getRandomLower());
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+console.log(getRandomNumber());
