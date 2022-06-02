10bc7cd1e411e80524021402811f7f640f32f214
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-06-02 22:07:39 +0100

    just finished a slide button project

--- index.js
+++ index.js
@@ -1,78 +1,22 @@
-const resultEl = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard')
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelectorAll('#good')
+const cheap = document.querySelectorAll('#cheap')
+const fast = document.querySelectorAll('#fast')
 
-const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbols: getRandomSymbols
-}
+toggles.forEach(toggle => toggle.addEventListener('change', (e => doTheTrick(e.target))))
 
-clipboardEl.addEventListener('click', () => {
-    const textarea = document.createElement('textarea')
-    const password = resultEl.innerText
+function doTheTrick(theClickedOne) {
+    if (good.checked && cheap.checked && fast.check) {
+        if (good === theClickedOne) {
+            fast.checked = false
+        }
 
-    if (!password) { return }
+        if (cheap === theClickedOne) {
+            good.checked = false
+        }
 
-    textarea.value = password
-    document.body.appendChild(textarea)
-    textarea.select()
-    document.execCommand('copy')
-    textarea.remove()
-    alert('Password copied to clipboard!')
-})
-
-generateEl.addEventListener('click', () => {
-    const length = lengthEl.value
-    const hasLower = lowercaseEl.checked
-    const hasUpper = uppercaseEl.checked
-    const hasNumber = numbersEl.checked
-    const hasSymbol = symbolsEl.checked
-
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-})
-
-function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = ''
-    const typesCount = lower + upper + number + symbol
-    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
-
-    if (typesCount === 0) {
-        return ''
+        if (fast === theClickedOne) {
+            cheap.checked = false
+        }
     }
-
-    for (let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0]
-            generatedPassword += randomFunc[funcName]()
-        })
-    }
-
-    const finalPassword = generatedPassword.slice(0, length)
-
-    return finalPassword
-}
-
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-
-
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-
-function getRandomSymbols() {
-    const symbols = '@£$%%^&*(*()_+{}|"?:>'
-    return symbols[Math.floor(Math.random() * symbols.length)]
 }
