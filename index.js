1561e1102485c74ce2f897280da18afd2a61ad60
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-06-03 22:17:14 +0100

    almost done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const addBtn = document.getElementById("add")
 
-addBtn.addEventListener('click', () => addNewNote())
+addBtn.addEventListener('click', () => addNewNote('Hello world))
 
-function addNewNote(text = '') {
-    const note = document.createElement('div')
-    note.classList.add('note')
+            function addNewNote(text = '') {
+                const note = document.createElement('div')
+                note.classList.add('note')
 
-    note.innerHTML = `
+                note.innerHTML = `
 	<div class="tools">
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
@@ -15,14 +15,26 @@ function addNewNote(text = '') {
   <div class='main ${text ? "" : "hidden"}'></div>
   <textarea class='${text ? "hidden" : ""}'></textarea>`
 
-    const editBtn = note.querySelector('.edit')
-    const deleteBtn = note.querySelector('.delete')
-    const main = note.querySelector('.main')
-    const textArea = note.querySelector('.txtarea')
+                const editBtn = note.querySelector('.edit')
+                const deleteBtn = note.querySelector('.delete')
+                const main = note.querySelector('.main')
+                const textArea = note.querySelector('txtarea')
 
-    deleteBtn.addEventListener('click', () => {
-        note.remove()
-    })
 
-    document.body.appendChild(note)
-}
+                deleteBtn.addEventListener('click', () => {
+                    note.remove()
+                })
+
+                editBtn.addEventListener('click', () => {
+                    main.classList.toggle('hidden')
+                    textArea.classList.toggle('hidden')
+                })
+
+                textArea.addEventListener('input', (e) => {
+                    const { value } = e.target
+
+                    main.innerHTML = marked(value)
+                })
+
+                document.body.appendChild(note)
+            }
