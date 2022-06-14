02e7c923583fe9a226058c604b38b5a7fbf256a2
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-06-14 10:37:28 +0100

    all finished

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
+const wrapper = document.querySelector(".sliderwrapper")
+const menuItems = document.querySelectorAll(".menuItem")
 
-runAnimation()
-
-function resetDOM() {
-    counter.classList.remove('hide')
-    finalMessage.classList.remove('show')
-
-    nums.forEach((num) => {
-        num.classList.value = ''
-    })
-
-    nums(0).classList.add('in')
-}
-
-function runAnimation() {
-    nums.forEach((num, idx) => {
-        const nextToLast = nums.length - 1
-
-        num.addEventListener('animationend', (e) => {
-            if (e.animatedName === 'goIn' && idx !== nextToLast) {
-                num.classList.remove('in')
-                num.classList.add('out')
-            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-                num.nextElementSibling.classList.add('in')
-            } else {
-                counter.classList.add('hide')
-                finalMessage.classList.add('show')
-            }
-        })
-    })
-}
-
-replay.addEventListener('click', () => {
-    resetDOM()
-    runAnimation()
-})
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
