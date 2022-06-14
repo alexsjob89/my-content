7f92d5341924ba93c5e9bb6236eae5878ba38abf
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-06-14 23:01:28 +0100

    work on js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".sliderwrapper")
 const menuItems = document.querySelectorAll(".menuItem")
 
+let choosenProduct = products[0]
+
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
