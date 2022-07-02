f4067517056487f63bd98c256c5af2938dfb9b5f
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-07-02 15:29:02 +0300

    just finished a rpoject

--- index.js
+++ index.js
@@ -1,135 +1,52 @@
-let navbar = document.querySelector('.navbar')
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('.header .navbar');
 
-dacument.querySelector('#menu-bar').onclick = () => {
-    varbar.classList.toggle('active')
-}
-
-dacument.querySelector('#close').onclick = () => {
-    varbar.classList.remove('active')
-}
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
 
 window.onscroll = () => {
-
+    menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+};
 
-    if (window.scrollY > 100) {
-
-        document.querySelector('header').classList.add('active';)
-    } else {
-        document.querySelector('header').classList.remove('active';)
-    }
-}
-
-let themeToggler = document.querySelector('#theme-toggler');
-
-themeToggler.onclick = () => {
-    themeToggler.classList.contains('fa-sun');
-    if (themeToggler.classList.contains('fa-sun')) {
-
-        document.querySelector('body').classList.add('active');
-    } else {
-
-        document.querySelector('body').classList.remove('active');
-
-    }
-
-}
-
-document.querySelectorAll('.small-image-1').forEach(images => {
-    images.onclick = () => {
-        document.querySelector('.big-image-1').src = images.getAttribute('src');
-    }
-})
-
-document.querySelectorAll('.small-image-2').forEach(images => {
-    images.onclick = () => {
-        document.querySelector('.big-image-2').src = images.getAttribute('src');
-    }
-})
-
-document.querySelectorAll('.small-image-3').forEach(images => {
-    images.onclick = () => {
-        document.querySelector('.big-image-3').src = images.getAttribute('src');
-    }
-});
-
-let countDate = new Date('aug 1, 2021 00:00:00').getTime();
-
-function countDown() {
-
-    let now = new Date().getTime();
-    gap = countDate - now;
-
-    let seconds = 1000;
-    let minutes = seconds * 60;
-    let hours = minutes * 60;
-    let days = hours * 24;
-
-    let d = Math.floor(gap / (days));
-    let h = Math.floor((gap % (days)) / (hours));
-    let m = Math.floor((gap % (hours)) / (minutes));
-    let s = Math.floor((gap % (minutes)) / (seconds));
-
-    document.getElementById('days').innerText = d;
-    document.getElementById('hours').innerText = h;
-    document.getElementById('minutes').innerText = m;
-    document.getElementById('seconds').innerText = s;
-}
-
-setInterval(funcation() {
-    countDown()
-}, 1000);
-
-var swiper = new Swiper(".product-slider", {
-    slidesPerView: 3,
+var swiper = new Swiper(".home-slider", {
     loop: true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-            slidesPerView: 2,
-        },
-        800: {
-            slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
-
+        nextEl: ".swiper-button.prev",
     },
 });
 
-var swiper = new Swiper(".review-slider", {
-    slidesPerView: 3,
+var swiper = new swiper(".reviews-slider", {
+    grabCursor: true,
     loop: true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
+    autoHeight: true,
+    spaceBetween: 20,
     breakpoints: {
         0: {
             slidesPerView: 1,
         },
-        550: {
+        700: {
             slidesPerView: 2,
-        },
-        800: {
-            slidesPerView: 3,
         },
         1000: {
             slidesPerView: 3,
         },
-
     },
 });
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+    for (var i = currentItem; i < currentItem + 3; i++) {
+        boxes[i].getElementsByClassName.display = 'inline-block';
+    };
+    currentItem += 3;
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.getElementsByClassName.display = 'none';
+    }
+}
