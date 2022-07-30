181cda3c63672f76955454b19116ff1b138d7369
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-07-30 23:57:05 +0100

    just created a slideShow

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let searchForm = document.querySelector('.search-form');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-let shoppingCart = document.querySelector('.shopping-cart');
-
-document.querySelector('#cart-btn').onclick = () => {
-    shoppingCart.classList.toggle('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-let loginForm = document.querySelector('.login-form');
-
-document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    navbar.classList.remove('active');
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
