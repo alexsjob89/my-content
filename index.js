03e0116b423785cdc25c836f78f8a91d5a7b3720
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-01 02:39:25 +0100

    a basic slideshow

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slider_img = document.querySelector('.slider-img');
+var market = ['sound.jpg', 'coffee.jpg', 'workbench.jpg']
+var i = 0;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function prev() {
+    if (i <= 0) i = market.length;
+    i--;
+    return setImg();
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function next() {
+    if (i >= market.length - 1) i = -1;
+    i++;
+    return setImg();
 }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+function setImg() {
+    return slider_img.setAttribute('src', 'market/' + market[i]);
 }
