545d70c2c7525078463034a0fe41b6ccaaad4a76
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-16 01:18:12 +0100

    functions

--- index.js
+++ index.js
@@ -65,4 +65,103 @@
 // console.log(sum);
 
 
-let x = 7;
+// let x = 7;
+
+// console.log(
+//     x > 10, x > 6
+// );
+
+// console.log(
+//     x >= 10, x >= 7
+// );
+
+// console.log(
+//    x < 10, x < 6
+// );
+
+// console.log(
+//    x <= 10, x <= 7
+// );
+
+// console.log(
+//     x != 7
+// )
+
+// let y = "7";
+
+// console.log(
+//     x === 7, x === y
+// )
+
+// console.log(
+//     x !== y
+// );
+
+// console.log(
+//     'A' < 'Z'
+// )
+
+// let a = 'Time'
+// let b = 'Timing'
+
+// console.log(
+//     a > b, 'e' > 'i'
+// );
+
+
+// let age = 18;
+
+// if(age >= 18) {
+//     console.log("you can vote");
+// } else {
+//     console.log("Sorry you can't vote");
+// }
+
+// age >= 18 ? [true] : [false];
+
+// let message = age >= 18 ? "You can vote" : "Sorry, you can't vote";
+
+// console.log(message);
+
+
+
+// let password = "Alex8989";
+
+// let message = password === "Alex8989" ? "success" : "fail";
+
+// console.log(message);
+
+
+
+// let body = document.querySelector("body");
+// let choice = "red";
+
+// choice === "red" ? body.classList.add("darkMode") : null;
+
+
+// let message = 1 >= 10 ? "condition is true" : "condition is false"
+
+// console.log(message);
+
+
+// let time = "7:00"
+
+// let greeting = time < "12:00" ? "good morning" : "good evening";
+
+// console.log(greeting);
+
+
+// let btn = document.querySelector("#btn");
+// let p = document.querySelector("p")
+
+// btn.addEventListener("click", function() {
+//     console.log("p");
+// });
+
+
+
+// function myName() {
+//     console.log("alex", "elisa", "eveline");
+// }
+
+// myName()
