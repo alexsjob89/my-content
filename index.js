bde61b1921b790615365d5d16a54d6a2f562d235
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-18 23:46:16 +0100

    practising

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// let supers = "superman, batman, flash";
-// let supersArray = supers.split(', ')
+var myNum;
 
-// console.log(supersArray);
+// myNum = Math.round(3.5);
+// myNum = Math.floor(3.9);
+// mytNum = Math.ceil(3.1);
+// myNum = Math.pow(3, 3);
+// myNum = Math.sqrt(63);
+// myNum = Math.abs(-1);
+// myNum = Math.min(1, 6, 4, 2);
+// myNum = Math.max(1, 5, 4, 3);
+// myNum = Math.PI;
+// myNum = Math.E;
 
-let supers = ['superman', 'batman', 'flash', 'aquaman', 'spider-man', 'wolfrine', 'cyclop', 'profer-x'];
-
-
-let findSuper = supers.findIndex(
-    function(value) {
-        return value === 'bigman'
-    }
-);
-
-
-console.log(findSuper);
+// console.log(myNum);
