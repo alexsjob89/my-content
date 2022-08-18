24fe6035b0249e7d06ba073a32f5c94a43e2e5b4
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-18 20:00:18 +0100

    exercises

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-let supers = ['superman', 'batman', 'flash', 'aquaman'];
+// let supers = "superman, batman, flash";
+// let supersArray = supers.split(', ')
 
-console.log(supers.join(' '));
+// console.log(supersArray);
+
+let supers = ['superman', 'batman', 'flash', 'aquaman', 'spider-man', 'wolfrine', 'cyclop', 'profer-x'];
+
+
+let findSuper = supers.findIndex(
+    function(value) {
+        return value === 'bigman'
+    }
+);
+
+
+console.log(findSuper);
