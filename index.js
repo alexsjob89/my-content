5333e34026e035a7edd7a576a6d831c9be819df8
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-19 22:54:57 +0100

    learning js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var myNum;
+const fruits = new Set();
 
-// myNum = Math.round(3.5);
-// myNum = Math.floor(3.9);
-// mytNum = Math.ceil(3.1);
-// myNum = Math.pow(3, 3);
-// myNum = Math.sqrt(63);
-// myNum = Math.abs(-1);
-// myNum = Math.min(1, 6, 4, 2);
-// myNum = Math.max(1, 5, 4, 3);
-// myNum = Math.PI;
-// myNum = Math.E;
+fruits.add('apple');
+fruits.add('banana');
+fruits.add('orange');
+fruits.add('plum').add('pear').add('apricots');
 
-// console.log(myNum);
+fruits.add('apple');
+
+fruits.delete('orange');
+
+let check = fruits.has('apple');
+
+console.log(fruits);
+
+console.log(fruits);
+console.log(check);
+console.log(fruits.size);
