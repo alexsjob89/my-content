25d082b0fb97833a4af89cb8c7d972028f424c7c
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-25 23:33:47 +0100

    progressing

--- index.js
+++ index.js
@@ -38,6 +38,24 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div')
+        div.className = `alert alert-${className}`;
+
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -47,11 +65,22 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
 
-    const title = document.querySelector('#title').value
-    const author = document.querySelector('#author').value
-    const isbn = document.querySelector('#isbn').value
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
 
     const book = new Book(title, author, isbn);
 
-    console.log(book);
+    if (title === '' || author === '' || isbn === '') {
+        alert('please fill in all fields');
+    } else {
+        UI.addBookToList(book);
+
+
+        UI.clearFields();
+    }
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
 });
