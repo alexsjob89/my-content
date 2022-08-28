4b1b5cb756c458eb420a3d5c463af847196e07b3
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-28 01:57:16 +0100

    still practice

--- index.js
+++ index.js
@@ -1,86 +1,18 @@
-class Book {
-    constructor(title, author, isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
+const theParent = document.querySelector('#moreBtn');
+const contentPara = document.querySelector('.content');
+// theParent.addEventListener('click', doSomething, false);
+
+// function doSomething(e) {
+//     if (e.target !== e.currentTarget) {
+//         var clickedItem = e.target.id;
+//         // alert('helllo' + clickedItem);
+//     }
+
+//     console.log(theParent, 'clicked');
+// }
+
+changeColor(contentPara)
+
+function changeColor(contentPara) {
+    contentPara.style.backgroundColor = 'red';
 }
-
-class UI {
-    static displayBooks() {
-        const StoredBooks = [{
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '3434434'
-            },
-            {
-                title: 'Book Two',
-                author: 'John Doe',
-                isbn: '45545'
-            }
-        ];
-        const books = StoredBooks;
-
-        books.forEach((book) => UI.addBookToList(book));
-    }
-
-    static addBookToList(book) {
-        const list = document.querySelector('#book-list');
-
-        const row = document.createElement('tr');
-
-        row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</td>
-        `;
-
-        list.appendChild(row);
-    }
-
-    static deleteBook(el) {
-        if (el.classList.contains('delete')) {
-            el.parentElement.parentElement.remove();
-        }
-    }
-
-    static showAlert(message, className) {
-        const div = document.createElement('div')
-        div.className = `alert alert-${className}`;
-
-    }
-
-    static clearFields() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-        document.querySelector('#isbn').value = '';
-    }
-}
-
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
-
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-
-    e.preventDefault()
-
-
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
-
-    const book = new Book(title, author, isbn);
-
-    if (title === '' || author === '' || isbn === '') {
-        alert('please fill in all fields');
-    } else {
-        UI.addBookToList(book);
-
-
-        UI.clearFields();
-    }
-});
-
-document.querySelector('#book-list').addEventListener('click', (e) => {
-    UI.deleteBook(e.target)
-});
