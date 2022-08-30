996d699f56c16b132a66ccff09220246626eebe5
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-08-31 00:25:31 +0100

    learning js

--- index.js
+++ index.js
@@ -5,7 +5,65 @@
 //     container[i].style.fontSize = '40px';
 //     container[i].style.color = 'blue';
 // }
-let ul = document.querySelector('ul')
 
-console.log(ul.parentNode);
-console.log(ul.parentElement);
+// const buttonOne = document.querySelector('.box-1')
+// const buttonTwo = document.querySelector('.box-2')
+// const buttonTree = document.querySelector('.box-3')
+
+
+// function changeBgColor() {
+//     buttonOne.style.backgroundColor = 'blue';
+// }
+
+// buttonOne.addEventListener('mouseover', changeBgColor);
+
+// function changeBgColor() {
+//     buttonTwo.style.backgroundColor = 'blue';
+// }
+
+// buttonTwo.addEventListener('mouseover', changeBgColor);
+
+// function changeBgColor() {
+//     buttonTree.style.backgroundColor = 'blue';
+// }
+
+// buttonTree.addEventListener('mouseover', changeBgColor);
+
+// const revealBtn = document.querySelector('.reveal-btn');
+// const hiddenContent = document.querySelector('.hidden-content');
+
+// hiddenContent.style.display = 'none';
+
+// function revealContent() {
+//     if (hiddenContent.style.display = 'block') {
+//         hiddenContent.style.display = 'none'
+//     } else {
+//         hiddenContent.style.display = 'block'
+//     }
+// }
+
+// revealBtn.addEventListener("click", revealContent);
+
+
+// const ages = [4, 12, 16, 20];
+
+// function checkAge(age) {
+//     return age >
+//         document.getElementById("ageToCheck").value;
+// }
+
+// function myFunction() {
+//     document.getElementById("fruits").innerHTML = ages.find(checkAge);
+// }
+
+let text = "";
+const fruits = ["apple", "orange", "cherry"];
+fruits.forEach(myFunction);
+
+document.getElementById("fruits").innerHTML = text;
+
+function myFunction(item, index) {
+    text += index + ": " + item + "<br>";
+
+
+}
