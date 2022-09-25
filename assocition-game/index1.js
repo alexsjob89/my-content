e0a6ec1432a549a0f9dc9a166a49c0a8e8357536
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-09-25 22:39:27 +0100

    exercises

--- assocition-game/index1.js
+++ assocition-game/index1.js
@@ -2,6 +2,13 @@
 const createAccount = document.querySelector('#create-account')
 const getStarted = document.querySelector('#getStarted')
 
+const logIn = document.querySelector('#login')
+const loginSign = document.querySelector('#loginSign')
+
+const submit = document.querySelector('#submit')
+const logButton = document.querySelector('#logButton')
+
+
 createAccount.style.display = 'none'
 
 getStarted.onclick = () => {
@@ -11,3 +18,25 @@ getStarted.onclick = () => {
         createAccount.style.display = 'none';
     }
 };
+
+
+
+loginSign.style.display = 'none'
+
+logIn.onclick = () => {
+    if (loginSign.style.display === 'none') {
+        loginSign.style.display = 'block';
+    } else {
+        loginSign.style.display = 'none';
+    }
+};
+
+//thill
+createAccount.addEventListener("create-account", (e) => {
+    e.preventDefault();
+    submit.addEventListener("submit", myFunction);
+
+    function myFunction() {
+        alert("the form was submitted!!")
+    }
+})
