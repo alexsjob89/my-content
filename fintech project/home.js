e70579094128f894bc3bc97578afa8c73143b0ea
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-10-19 18:38:55 +0100

    first box

--- fintech project/home.js
+++ fintech project/home.js
@@ -8,3 +8,18 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+// cards animate
+
+var tl = gsap.timeline({ repeat: Infinity, repeatDelay: 1, yoyo: true });
+tl.to(".one", { x: -20, duration: 1 });
+tl.to(".two", { x: 20, duration: 1 });
+tl.to(".tree", { x: -20, duration: 1 });
+tl.to(".four", { x: 20, duration: 1 });
+tl.to(".five", { x: -20, duration: 1 });
+tl.to(".six", { x: 20, duration: 1 });
+tl.to(".seven", { x: -20, duration: 1 });
+tl.to(".eight", { x: 20, duration: 1 });
+tl.to(".nine", { x: -20, duration: 1 });
+
+gsap.fromTo([".imagesCards", ".background"], { x: 140, opacity: 0.2, duration: 3, repeat: 3 }, { x: -10, opacity: 0.7, duration: 3, repeat: 3 })
