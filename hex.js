69226bec311125bd880c4d3915a271650d8438b0
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-10-30 17:06:09 +0000

    doing some exercises

--- hex.js
+++ hex.js
@@ -1,4 +1,4 @@
-// gsap.fromTo(".ball1", { x: -30, duration: 3 }, { x: 20, duration: 30 });
-
-// const tl = gsap.timeline();
-// tl.fromTo(".ball1", { x: 100, duration: 3, }, { x: -100, duration: 3 });
+function myFunction() {
+    var myWindow = window.open("", "", "width=500, height=300");
+    myWindow.blur();
+}
