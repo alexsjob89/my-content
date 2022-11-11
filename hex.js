f39062a72ba66c73f85ad2eb00c4267e4b2f9786
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-11-11 20:31:40 +0000

    some js practice

--- hex.js
+++ hex.js
@@ -1,9 +1,40 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    id: 5566,
-    fullName: function() {
-        return this.firstName + " " + this.lastName;
-    }
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     fullName: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+// console.log(person);
+
+
+// document.addEventListener("mouseover", myFunction);
+// document.addEventListener("click", mySecondFunction);
+// document.addEventListener("mouseout", myThirdFunction);
+
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "hello i am a Developer!";
+// }
+
+// function mySecondFunction() {
+//     document.getElementById("demo").innerHTML = "clicked!"
+//     document.body.style.background = "none";
+
+// }
+
+// function myThirdFunction() {
+//     document.getElementById("demo").innerHTML = "mouse uot!";
+//     document.body.style.backgroundColor = "red";
+
+// }
+
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+    const frame = document.getElementById("myFrame");
+    const h1 = frame.contentWindow.document.getElementByTagNme("h1")[0];
+    const node = document.adaptNode(h1);
+    document.body.appendChild(node);
 };
-console.log(person);
