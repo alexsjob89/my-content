325ee5a123caf5c7937b5b41fcff3a74295ba62b
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2022-12-19 01:27:35 +0000

    exercises with js

--- notes-app/js/main.js
+++ notes-app/js/main.js
@@ -1,21 +1,120 @@
-// const nodeMap = document.getElementById("myImg").attributes;
+// const btnBox = document.querySelector("#two")
+// const btnthree = document.querySelector("#three")
+// const btnfour = document.querySelector("#four")
+// const btnfive = document.querySelector("#five")
 
-// let text = "";
-// for (let i = 0; i < nodeMap.length; i++) {
-//     text += nodeMap[i].name + " = " + nodeMap[i].value + "<br>";
+// const boxt = document.querySelector(".Two-box")
+// const boxThree = document.querySelector(".Three-box")
+// const boxFour = document.querySelector(".Four-box")
+// const boxFive = document.querySelector(".Five-box")
+
+// const btnboxs = document.querySelectorAll("#boxes-content")
+// const boxesbox = document.querySelectorAll(".box")
+
+// btnBox.addEventListener("click", () => {
+//     if (boxt.style.display === "block") {
+//         boxt.style.display = "none"
+//     } else {
+//         boxt.style.display = "block";
+//     }
+// });
+
+
+
+// btnthree.addEventListener("click", () => {
+//     if (boxThree.style.display === "block") {
+//         boxThree.style.display = "none";
+//     } else {
+//         boxThree.style.display = "block";
+//     }
+
+// });
+
+// btnfour.addEventListener("click", () => {
+//     if (boxFour.style.display === "block") {
+//         boxFour.style.display = "none";
+//     } else {
+//         boxFour.style.display = "block";
+//     }
+
+// });
+
+// btnfive.addEventListener("click", () => {
+//     if (boxFive.style.display === "block") {
+//         boxFive.style.display = "none";
+//     } else {
+//         boxFive.style.display = "block";
+//     }
+
+// });
+
+
+
+
+// function show() {
+//     document.getElementById("content").style.height = "400px";
+//     document.getElementById("content").style.display = "block";
+//     document.getElementById("show").style.display = "none";
 // }
-// document.getElementById("demo").innerHTML = text;
 
-var tween = gsap.fromTo(".one", {
-    duration: 4,
-    x: 750,
-    ease: "none",
-    paused: true
-}, { x: 200 });
+// function hide() {
+//     document.getElementById("content").style.height = "0px";
+//     document.getElementById("content").style.display = "none";
+//     document.getElementById("show").style.display = "inline";
+// }
 
-// click handlers for controlling the tween instance...
-document.querySelector("#play").onclick = () => tween.play();
-document.querySelector("#pause").onclick = () => tween.pause();
-document.querySelector("#resume").onclick = () => tween.resume();
-document.querySelector("#reverse").onclick = () => tween.reverse();
-document.querySelector("#restart").onclick = () => tween.restart();
+
+// const names = ["jhon", "frank", "ben", "clark", "kent"]
+// const greetings = ["good morning", "how are you today?"]
+
+// for (name of names)
+
+//     console.log(`my name is ${name}, ${greetings}`);
+
+// const content = document.getElementById("content");
+// const button = document.getElementById("btn");
+
+// button.onclick = function() {
+//     if (content.className === "open") {
+//         content.className = "";
+//         button.innerHTML = "show more";
+
+//     } else {
+//         content.className = "open";
+//         button.innerHTML = "show less";
+//     }
+// };
+
+// const main = document.querySelector("main");
+
+// let htmlString = "";
+
+// for (let i = 0; i < 100; i++) {
+//     htmlString += `<div>${[i]}</div>`;
+
+// }
+// main.innerHTML = htmlString;
+
+
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+const lione = document.createElement("li");
+const litwo = document.createElement("li");
+const lithree = document.createElement("li");
+const H1 = document.createElement("h1");
+
+ul.append(H1, li, lione, litwo, lithree);
+
+H1.innerText = "movieBox";
+li.innerText = "movies";
+lione.innerText = "foodBox";
+litwo.innerText = "drinks";
+lithree.innerText = "fun";
+
+ul.classList.add("main-list-header");
+li.classList.add("main-list");
+lione.classList.add("main-one");
+litwo.classList.add("main-two");
+lithree.classList.add("main-three");
+
+console.log(ul);
